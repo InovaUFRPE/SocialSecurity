0f752f2974e9ebd97eebdca72a1a0961ffcfa61e
jmontejr <josecmontejr@gmail.com>
2017-12-30 02:36:18 -0200

    Atividade #608 - Ajustar o perfil
    Perfil consumindo dados da API
    Pagina com reload para modificacoes

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ContactPage } from '../contact/contact';
+import { EditProfilePage } from '../edit-profile/edit-profile';
+import { UniqueDeviceID } from '@ionic-native/unique-device-id';
+import { UsersController } from '../../providers/usuario/users-controller/users-controller';
+import { RegisterContactPage } from '../register-contact/register-contact';
 
 @IonicPage()
 @Component({
@@ -9,15 +13,43 @@ import { ContactPage } from '../contact/contact';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  user = {
+    nome_usuario: "",
+    email_usuario: ""
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private uniqueDeviceID: UniqueDeviceID,
+    private userController: UsersController) {
+      
+  }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter ProfilePage');
+    this.userInfo()
+  }
+
+  public userInfo():void{
+    this.uniqueDeviceID.get().then(udid => {
+      this.userController.verifyDevice(udid).then((res: any) => {
+        if (res.data.status_log == "logged") {
+          this.userController.getUser(res.data.cod_usuario).then((res: any) => {
+            this.user.nome_usuario = res.data.nome_usuario.split(" ")[0]
+            this.user.email_usuario = res.data.email_usuario.split(" ")[0];
+          })
+        }
+      })
+    })
+  }
+
+  public toEditProfile():void{
+    this.navCtrl.push(EditProfilePage);
   }
 
   public toContactPage():void{
-    this.navCtrl.push(ContactPage)
+    this.navCtrl.push(ContactPage);
   }
 
 }
